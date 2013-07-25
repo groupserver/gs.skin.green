7e25dd7082e5dae97fddcb2ebef6d5da84cecdd6
Michael JasonSmith <mpj17@onlinegroups.net>
2013-07-25 11:43:15 -0700

    Code cleanup, helped by Ninja-IDE

--- gs/skin/green/interfaces.py
+++ gs/skin/green/interfaces.py
@@ -1,12 +1,14 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.app.rotterdam import Rotterdam
 
-# Layers just inherit from IBrowserRequest.
+
 class IGSGreenLayer(IBrowserRequest):
     '''GroupServer Green Layer'''
-# Skins inherit from a bunch of layers. In this case the green layer
-# and the core Zope Rotterdam layer.
+    # Layers just inherit from IBrowserRequest.
+
+
 class IGSGreenSkin(IGSGreenLayer, Rotterdam):
     '''GroupServer Green Skin'''
-
+    # Skins inherit from a bunch of layers. In this case the green layer
+    # and the core Zope Rotterdam layer.
